29fe273ebea405da47e01eb6126c0187032e6de0
Naman-77 <63945865+Naman-77@users.noreply.github.com>
2021-05-23 11:55:10 +0530

    Add files via upload

---
+++ ranting-popcorn/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Text, View, StyleSheet} from 'react-native'
+import WelcomeScreen from './screens/WelcomeScreen'
+
+export default function App(){
+  return(
+    <WelcomeScreen/>
+  );
+}
---
+++ ranting-popcorn/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDLfuL9Diq2mXZvuYdivA_L02LaqH1zw8Q",
+  authDomain: "booksanta-b080f.firebaseapp.com",
+  projectId: "booksanta-b080f",
+  storageBucket: "booksanta-b080f.appspot.com",
+  messagingSenderId: "148397103332",
+  appId: "1:148397103332:web:9762232665b654590ab4f1"
+};
